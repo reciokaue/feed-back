1f905522981c71485be23c9d5e6096145baef4c1
reciokaue <kaue.recio2@gmail.com>
2024-03-06 19:59:40 -0300

    get all forms pagination

--- src/routes/forms/getAll.ts
+++ src/routes/forms/getAll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { statusMessage } from '../../utils/statusMessage'
 import { jwtUser } from '../../types/jwtUser'
 import { prisma } from '../../lib/prisma'
+import { z } from 'zod'
 
 export async function getAllForms(app: FastifyInstance) {
   app.addHook('onRequest', async (request, reply) => {
@@ -15,12 +16,20 @@ export async function getAllForms(app: FastifyInstance) {
   app.get('/form', async (request) => {
     const user = request.user as jwtUser
 
+    const paramsSchema = z.object({
+      page: z.number().default(0),
+      pageSize: z.number().default(6),
+    })
+    const { page, pageSize } = paramsSchema.parse(request.params)
+
     const form = await prisma.form.findMany({
       orderBy: { id: 'asc' },
       where: {
         userId: user.sub,
       },
       include: { _count: true },
+      take: pageSize,
+      skip: page * pageSize,
     })
 
     return form
