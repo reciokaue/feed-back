5e8f8e8eee3c24617e054212fec42b8dbc992327
reciokaue <kaue.recio2@gmail.com>
2024-03-07 19:51:36 -0300

    forms pagination fix

--- src/routes/forms/getAll.ts
+++ src/routes/forms/getAll.ts
@@ -3,6 +3,7 @@ import { statusMessage } from '../../utils/statusMessage'
 import { jwtUser } from '../../types/jwtUser'
 import { prisma } from '../../lib/prisma'
 import { z } from 'zod'
+import { paginationSchema } from '../../utils/schemas/pagination'
 
 export async function getAllForms(app: FastifyInstance) {
   app.addHook('onRequest', async (request, reply) => {
@@ -16,22 +17,20 @@ export async function getAllForms(app: FastifyInstance) {
   app.get('/form', async (request) => {
     const user = request.user as jwtUser
 
-    const paramsSchema = z.object({
-      page: z.number().default(0),
-      pageSize: z.number().default(6),
-    })
-    const { page, pageSize } = paramsSchema.parse(request.params)
+    const { page, pageSize, isDefault } = paginationSchema.parse(request.query)
+    console.log(request.query)
+    console.log(page, pageSize, isDefault)
 
-    const form = await prisma.form.findMany({
-      orderBy: { id: 'asc' },
+    const forms = await prisma.form.findMany({
       where: {
-        userId: user.sub,
+        ...(!isDefault && { userId: user.sub }),
+        isDefault,
       },
       include: { _count: true },
       take: pageSize,
       skip: page * pageSize,
     })
 
-    return form
+    return forms
   })
 }
---
+++ src/utils/schemas/pagination.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+export const paginationSchema = z.object({
+  page: z
+    .string()
+    .transform((val) => +val)
+    .default('0'),
+  pageSize: z
+    .string()
+    .transform((val) => +val)
+    .default('6'),
+  isDefault: z
+    .string()
+    .transform((val) => val.toLowerCase() === 'true')
+    .default('false'),
+})
